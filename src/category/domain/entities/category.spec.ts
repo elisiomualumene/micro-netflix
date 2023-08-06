c482c49dd761e416554e40210669c751e35ec3fe
Elísio Mualumene <85412882+elisiomualumene@users.noreply.github.com>
2023-08-06 23:49:52 +0000

    :fire: category tests

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,18 +1,64 @@
-import { Category } from "./category"
+import { Category } from "./category";
+import { omit } from "lodash";
 
 describe("Category Test", () => {
-    it("should be able to create a category entity", () => {
-        // triple AAA - Arrange Act Assert
+  it("should be able to create a category entity", () => {
+    let category = new Category({ name: "Movie" });
 
-        const props = {
-            name: "Movie", 
-            description: "description",
-             is_active: true,
-            created_at: new Date()
-        }
+    let props = omit(category.props, "created_at");
 
-        const category = new Category(props)
+    expect(props).toStrictEqual({
+      name: "Movie",
+      description: undefined,
+      is_active: true,
+    });
 
-        expect(category).toStrictEqual({props})
-    })
-})
+    expect(category.props.created_at).toBeInstanceOf(Date);
+
+    category = new Category({
+      name: "Movie",
+      description: "some description",
+      is_active: false,
+    });
+
+    let created_at = new Date();
+
+    expect(category.props).toStrictEqual({
+      name: "Movie",
+      description: "some description",
+      is_active: false,
+      created_at,
+    });
+
+    category = new Category({
+      name: "Movie",
+      description: "other description",
+    });
+
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      description: "other description",
+    });
+
+    category = new Category({
+      name: "Movie",
+      is_active: true,
+    });
+
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      is_active: true,
+    });
+
+    created_at = new Date();
+    category = new Category({
+      name: "Movie",
+      created_at,
+    });
+
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      created_at,
+    });
+  });
+});
--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,27 +1,38 @@
-
 export type ICategoryProps = {
-    name: string;
-    is_active?: boolean;
-    description?: string;
-    created_at?: Date;
-}
+  name: string;
+  is_active?: boolean;
+  description?: string;
+  created_at?: Date;
+};
 
 export class Category {
-    constructor(public readonly props: ICategoryProps){}
+  constructor(public readonly props: ICategoryProps) {
+    this.props.description = this.props.description;
+    this.props.is_active = this.props.is_active ?? true;
+    this.props.created_at = this.props.created_at ?? new Date();
+  }
 
-    get name(): string{
-        return this.props.name
-    }
+  get name(): string {
+    return this.props.name;
+  }
 
-    get description(): string | undefined{
-        return this.props.description
-    }
+  get description(): string | undefined {
+    return this.props.description;
+  }
 
-    get is_active(): boolean | undefined{
-        return this.props.is_active
-    }
+  private set description(description: string) {
+    this.props.description = description ?? null;
+  }
 
-    get created_at(): Date | undefined{
-        return this.props.created_at
-    }
+  get is_active(): boolean | undefined {
+    return this.props.is_active;
+  }
+
+  private set is_active(is_active: boolean) {
+    this.props.is_active = is_active ?? null;
+  }
+
+  get created_at(): Date | undefined {
+    return this.props.created_at;
+  }
 }
