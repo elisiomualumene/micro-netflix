f1e10e02c230362b16e35e6e2b387c28945266f7
elisiomualumene <elisiomualumene@gmail.com>
2023-08-09 19:17:31 +0100

    refactor: testing value object immutability

--- src/shared/domain/value-objects/__tests__/value-object.spec.ts
+++ src/shared/domain/value-objects/__tests__/value-object.spec.ts
@@ -1,3 +1,4 @@
+import { deepFreeze } from "../../utils/object";
 import ValueObject from "../value-object";
 
 class StubValueObject extends ValueObject {}
@@ -9,5 +10,40 @@ describe("ValueObject Unit Test", () => {
 
         vo = new StubValueObject({prop1: "value1"})
         expect(vo.value).toStrictEqual({prop1: "value1"})
+    })
+
+    it("should be able to convert to string", () => {
+        const date = new Date()
+        let arrange = [
+            {received:"", expected: ""},
+            {received:"fake test", expected: "fake test"},
+            {received:0, expected: "0"},
+            {received:1, expected: "1"},
+            {received:5, expected: "5"},
+            {received: true, expected: "true"},
+            {received:false, expected: "false"},
+            {received: date, expected: date.toString()},
+            {received: {prop1: "value1"}, expected: JSON.stringify({prop1: "value1"})}
+        ]
+
+        arrange.forEach(value => {
+            const vo = new StubValueObject(value.received)
+            expect(vo+"").toBe(value.expected)
+        })
+    })
+
+    it("should be able immutable object", () => {
+        const obj = deepFreeze({prop1: "value1", deep: {prop2: "value2", prop3: new Date()}})
+        const vo = new StubValueObject(obj)
+        
+        expect(() => {
+            (vo as any).value.prop1 = "test"
+        }).toThrow("Cannot assign to read only property 'prop1' of object '#<Object>'")
+
+        expect(() => {
+            (vo as any).value.deep.prop2 = "test"
+        }).toThrow("Cannot assign to read only property 'prop2' of object '#<Object>'")
+
+        expect(vo.value.deep.prop3).toBeInstanceOf(Date)
     })
 })
